617d95426c7f9deb701b3dd8fac756ff2b67ce40
Subham  Sahu <subham.sahu@DOCQIN044.tail8e4de.ts.net>
2023-03-23 16:24:02 +0530

    fetching multiple pages, infinite scroll

--- src/components/CustomReactComponents.jsx
+++ src/components/CustomReactComponents.jsx
@@ -74,70 +74,116 @@
 //   );
 // }
 
-import React, { useEffect, useRef, useState, useCallback } from "react";
-import { WindowScroller } from "react-virtualized";
-import { VariableSizeList } from "react-window";
-import Card from "./Card/Card";
+// import React, { useEffect, useRef, useState, useCallback } from "react";
+// import { WindowScroller } from "react-virtualized";
+// import { VariableSizeList } from "react-window";
+// import Card from "./Card/Card";
 
-export default function CustomReactComponnets() {
-  const list = useRef(null);
-  const root = useRef(null);
+// export default function CustomReactComponnets() {
+//   const list = useRef(null);
+//   const root = useRef(null);
 
-  const [rowSizes, setRowSizes] = useState([]);
-  const [pageNo, setPageNo] = useState(1);
+//   const [rowSizes, setRowSizes] = useState([]);
+//   const [pageNo, setPageNo] = useState(1);
 
-  const fetcher = async (pageNo) => {
-    const response = await fetch(
-      `https://picsum.photos/v2/list?page=${pageNo}&limit=5`
-    );
-    const data = await response.json();
-    setRowSizes((prev) => [...prev, ...data]);
+//   const fetcher = async (pageNo) => {
+//     const response = await fetch(
+//       `https://picsum.photos/v2/list?page=${pageNo}&limit=5`
+//     );
+//     const data = await response.json();
+//     setRowSizes((prev) => [...prev, ...data]);
+//   };
+
+//   const onScroll = useCallback(({ scrollTop, scrollHeight, clientHeight }) => {
+//     list.current?.scrollTo(scrollTop);
+//   }, []);
+
+//   useEffect(() => {
+//     fetcher(1);
+//   }, []);
+
+//   const getItemSize = () => 400;
+
+//   return (
+//     <>
+//       <WindowScroller onScroll={onScroll}>{() => <div />}</WindowScroller>
+//       <VariableSizeList
+//         ref={list}
+//         itemCount={rowSizes.length}
+//         itemSize={getItemSize}
+//         width={window.innerWidth - 10}
+//         height={window.innerHeight - 80}
+//         style={{
+//           height: "100% !important",
+//           width: "100% !important",
+//           overflow: "unset !important",
+//         }}
+//       >
+//         {({ index, style }) => (
+//           <div ref={root} style={style}>
+//             <Card
+//               id={rowSizes[index].id}
+//               heading={rowSizes[index].heading}
+//               src={rowSizes[index].download_url}
+//               author={rowSizes[index].author}
+//             />
+//           </div>
+//         )}
+//       </VariableSizeList>
+//     </>
+//   );
+// }
+
+import React, { useState, useEffect } from "react";
+import Card from './Card/Card';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    // Fetch the data when the page changes
+    fetchData();
+  }, [page]);
+
+  const handleScroll = () => {
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+      // User has scrolled to the bottom of the page
+      setPage((prevPage) => prevPage + 1);
+    }
   };
 
-  const onScroll = useCallback(({ scrollTop, scrollHeight, clientHeight }) => {
-    // check if we're about to reach the end of the list
-    if (scrollTop + clientHeight >= scrollHeight - 50) {
-      setPageNo((prev) => prev + 1);
-    }
-    list.current?.scrollTo(scrollTop);
-  }, []);
-
-  useEffect(() => {
-    fetcher(1);
-  }, []);
-
-  useEffect(() => {
-    fetcher(pageNo+1);
-  }, [pageNo]);
-
-  const getItemSize = () => 400;
+  const fetchData = async () => {
+    // Fetch data from the API
+    const response = await fetch(
+      `https://picsum.photos/v2/list?page=${page}&limit=5`
+    );
+    const newData = await response.json();
+    setData((prevData) => [...prevData, ...newData]);
+    console.log(newData);
+  };
 
   return (
-    <>
-      <WindowScroller onScroll={onScroll}>{() => <div />}</WindowScroller>
-      <VariableSizeList
-        ref={list}
-        itemCount={rowSizes.length}
-        itemSize={getItemSize}
-        width={window.innerWidth - 10}
-        height={window.innerHeight - 80}
-        style={{
-          height: "100% !important",
-          width: "100% !important",
-          overflow: "unset !important",
-        }}
-      >
-        {({ index, style }) => (
-          <div ref={root} style={style}>
-            <Card
-              id={rowSizes[index].id}
-              heading={rowSizes[index].heading}
-              src={rowSizes[index].download_url}
-              author={rowSizes[index].author}
-            />
-          </div>
-        )}
-      </VariableSizeList>
-    </>
+    <div className="container">
+      {data.map((item) => (
+        <div key={item.url}>
+          {/* <img src={item.download_url} width={300} height={200} /> */}
+          <Card src={item.download_url} width={300} height={200}/>
+        </div>
+      ))}
+    </div>
   );
 }
+
+export default App;
